0866cf4df3f14790d8524dd578500307ecfc799c
kairu-chege <danielkairuchege@gmail.com>
2024-11-22 01:09:42 +0300

    week4

--- Practice/fileReadWriteErrorHandling.py
+++ Practice/fileReadWriteErrorHandling.py
@@ -2,11 +2,11 @@ import os # this module provides functions for interacting with the operating sy
 
 """this function modifies the contents of an input file and writes the modified
 content to an output file """
-def modify_file(NewDoc, myDatafile):
+def modify_file(input_file, output_file):
     
     try:
         #The with statement ensures that the files are closed properly, even if exceptions occur.
-        with open(NewDoc, 'r') as f_in, open(myDatafile, 'w') as f_out:
+        with open(input_file, 'r') as f_in, open(output_file, 'w') as f_out:
             for line in f_in:
               #modify the line here(capitalizes each word)
               modified_line = ' '.join(word.capitalize() for word in line.split())
@@ -14,7 +14,7 @@ def modify_file(NewDoc, myDatafile):
               f_out.write(modified_line)
     #handling of potential filenotfouderror and ioerror exceptions
     except FileNotFoundError:
-        print(f"Error: Input file {NewDoc}' not found")
+        print(f"Error: Input file {input_file}' not found")
     except IOError as e:
         print(f"Error: I/O error occured: {e} !")
         
